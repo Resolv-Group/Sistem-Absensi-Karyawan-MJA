ab42f47f8ed2febe7864cef31ff7b0865db1eb09
Sansan <sani.aliem@gmail.com>
2026-01-15 13:55:23 +0700

    Increase notification display duration to 7 seconds
    Updated notification component to show success and error messages for 7 seconds instead of 3. Also added custom validation error messages in bulkAbsensiBoronganUpdate and removed commented code for clarity.

--- resources/views/Absensi/main-absensi.blade.php
+++ resources/views/Absensi/main-absensi.blade.php
@@ -128,20 +128,7 @@
     {{-- ================================
     3. TOOLBAR & FILTERS
     ================================= --}}
-    {{-- Left: View Switcher (Visual only) --}}
-        {{-- <div class="bg-gray-100 p-1 rounded-lg inline-flex self-start sm:self-center">
-            <div
-                class="inline-flex items-center gap-2 px-3 py-1.5 bg-white border border-gray-200 rounded-full shadow-sm text-sm font-medium text-gray-600">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20"
-                    fill="currentColor">
-                    <path fill-rule="evenodd"
-                        d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                        clip-rule="evenodd" />
-                </svg>
-                <span>{{ \Carbon\Carbon::now()->translatedFormat('F Y') }}</span>
-            </div>
-        </div> --}}
-    <div 
+    <div
         x-data="{
             date: '{{ request('date') ?? now()->toDateString() }}',
             go() {
