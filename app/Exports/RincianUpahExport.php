85d14dc3dc86a181d60311753b34b9a3db2b4f96
NigelKus <143382300+NigelKus@users.noreply.github.com>
2026-02-05 18:43:59 +0700

    Add Rincian Upah Excel export
    Introduce Rincian Upah Excel export feature: add App\Exports\RincianUpahExport (FromView, WithEvents, WithColumnWidths) which sets column widths, inserts a top row/column, and renders data chunked by 3 via a new view resources/views/Exports/rincian-upah.blade.php. Add PayrollController::ExportRincianUpahBorongan to parse workers_json, eager-load Pekerja relations, build formatted items, format period, and return an Excel download. Update overview-payroll view to prepare workers JSON and add a POST form/button to export, and register a POST route (/test/export-rincian-upah-borongan). Also add the RincianUpahExport import to the controller and (note) a debug dd() was introduced in ExportDetailBorongan.

---
+++ app/Exports/RincianUpahExport.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Exports;
+
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
+class RincianUpahExport implements FromView, WithEvents, WithColumnWidths
+{
+
+    protected $data;
+    protected $periode;
+    public function __construct($data, $periode) 
+    {
+        $this->data = $data;
+        $this->periode = $periode;
+    }
+
+    public function view(): View
+    {
+        $chunks = $this->data->chunk(3);
+        return view('Exports.rincian-upah', [
+            'chunks'  => $chunks,
+            'periode' => $this->periode
+        ]);
+
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            1    => ['font' => ['bold' => true]],
+        ];
+    }
+
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 13,  
+            'B' => 2,
+            'C' => 2,
+            'D' => 2,
+            'E' => 4,
+            'F' => 6,
+            'G' => 11,
+            'H' => 2,
+            'I' => 18,
+            'J' => 2,
+
+            //2nd Row
+            'K' => 13,  
+            'L' => 2,
+            'M' => 2,
+            'N' => 2,
+            'O' => 4,
+            'P' => 6,
+            'Q' => 11,
+            'R' => 2,
+            'S' => 18,
+            'T' => 2,
+
+            //3rd Row
+            'U' => 13,  
+            'V' => 2,
+            'W' => 2,
+            'X' => 2,
+            'Y' => 4,
+            'Z' => 6,
+            'AA' => 11,
+            'AB' => 2,
+            'AC' => 18,
+            'AD' => 2,
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $event) {
+                // Menambahkan baris kosong di atas (Baris 1)
+                $event->sheet->insertNewRowBefore(1, 1);
+                
+                // Menambahkan kolom kosong di kiri (Kolom A)
+                $event->sheet->insertNewColumnBefore('A', 1);
+            },
+        ];
+    }
+}
---
+++ resources/views/Exports/rincian-upah.blade.php
@@ -0,0 +1,264 @@
+<table>
+    {{-- LOOPING VERTIKAL (Per Grup isi 3 Orang) --}}
+    @foreach($chunks as $group)
+
+        {{-- ========================================================== --}}
+        {{-- BAGIAN 1: HEADER & IDENTITAS --}}
+        {{-- ========================================================== --}}
+
+        {{-- BARIS 1: Header Judul --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="9" style="border: 1px solid #000000; border-bottom: none; background-color: #FCE4D6; font-weight: bold; text-align: center; font-size: 12pt; text-decoration: underline;">
+                    RINCIAN UPAH
+                </td>
+                <td style="width: 20px;"></td> {{-- SPACER ANTAR STRUK --}}
+            @endforeach
+        </tr>
+
+        {{-- BARIS 2: Nama --}}
+        <tr>
+            @foreach($group as $item)
+                <td style="border-left: 1px solid #000000; font-weight: bold; background-color: #FCE4D6;">NAMA</td>
+                <td style="text-align: center; background-color: #FCE4D6;">:</td>
+                <td colspan="7" style="border-right: 1px solid #000000; font-weight: bold; background-color: #FCE4D6;">{{ $item->nama }}</td>
+                <td></td> {{-- SPACER --}}
+            @endforeach
+        </tr>
+
+        {{-- BARIS 3: ID --}}
+        <tr>
+            @foreach($group as $item)
+                <td style="border-left: 1px solid #000000; font-weight: bold; background-color: #FCE4D6;">ID</td>
+                <td style="text-align: center; background-color: #FCE4D6;">:</td>
+                <td colspan="7" style="border-right: 1px solid #000000; font-weight: bold; text-align: left; background-color: #FCE4D6;">{{ $item->id_karyawan }}</td>
+                <td></td> {{-- SPACER --}}
+            @endforeach
+        </tr>
+
+        {{-- BARIS 4: Jabatan / Divisi --}}
+        <tr>
+            @foreach($group as $item)
+                <td style="border-left: 1px solid #000000; font-weight: bold; background-color: #FCE4D6;">JAB. / DIV.</td>
+                <td style="text-align: center; background-color: #FCE4D6;">:</td>
+                <td colspan="4" style="font-weight: bold; background-color: #FCE4D6;">{{ $item->jabatan }}</td>
+                <td style="background-color: #FCE4D6;"></td>
+                <td colspan="2" style="border-right: 1px solid #000000; font-weight: bold; text-align: right; background-color: #FCE4D6;">{{ $item->divisi }}</td>
+                <td></td> {{-- SPACER --}}
+            @endforeach
+        </tr>
+
+        {{-- BARIS 5: Periode --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="9" style="border: 1px solid #000000; background-color: #FCE4D6; font-weight: bold; text-align: center;">
+                    {{ $periode }}
+                </td>
+                <td></td> {{-- SPACER --}}
+            @endforeach
+        </tr>
+
+        {{-- ========================================================== --}}
+        {{-- BAGIAN 2: PENDAPATAN --}}
+        {{-- ========================================================== --}}
+
+        {{-- BARIS 6: Upah Pokok --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="7" style="border-left: 1px solid #000000;">UPAH POKOK</td>
+                <td style="text-align: center;">=</td>
+                <td style="border-right: 1px solid #000000; text-align: right;">{{ number_format($item->upah_pokok, 0, ',', '.') }}</td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS 7: Upah Lembur --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="4" style="border-left: 1px solid #000000;">UPAH LEMBUR</td>
+                <td colspan="2" style="border: 1px solid #000000; text-align: center;">{{ $item->lembur_jam }}</td>
+                <td style="border: 1px solid #000000; text-align: right;">{{ number_format($item->lembur_rate, 0, ',', '.') }}</td>
+                <td style="text-align: center;">=</td>
+                <td style="border-right: 1px solid #000000; text-align: right;">{{ number_format($item->total_lembur ?? 0, 0, ',', '.') }}</td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS 8: Upah Lembur HBN --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="4" style="border-left: 1px solid #000000;">UPAH LEMBUR HBN</td>
+                <td colspan="2" style="border: 1px solid #000000; text-align: center;">0</td>
+                <td style="border: 1px solid #000000; text-align: right;">{{ number_format($item->lembur_hbn_rate, 0, ',', '.') }}</td>
+                <td style="text-align: center;">=</td>
+                <td style="border-right: 1px solid #000000; text-align: right;">-</td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS 9: Insentif --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="4" style="border-left: 1px solid #000000;">UANG INSENTIF</td>
+                <td colspan="2" style="border: 1px solid #000000; text-align: center;">0</td>
+                <td style="border: 1px solid #000000; text-align: center;">-</td>
+                <td style="text-align: center;">=</td>
+                <td style="border-right: 1px solid #000000; text-align: right;">-</td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS 10: Tunjangan --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="4" style="border-left: 1px solid #000000;">UANG TUNJANGAN</td>
+                <td colspan="2" style="border: 1px solid #000000; text-align: center;">0</td>
+                <td style="border: 1px solid #000000; text-align: center;">-</td>
+                <td style="text-align: center;">=</td>
+                <td style="border-right: 1px solid #000000; text-align: right;">-</td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS 11: Spacer Garis Ganda --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="8" style="border-left: 1px solid #000000;"></td>
+                <td style="border-right: 1px solid #000000; border-bottom: 3px double #000000;"></td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS 12: Jumlah 1 --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="7" style="border-left: 1px solid #000000; font-weight: bold; font-style: italic; text-align: center;">JUMLAH 1</td>
+                <td></td>
+                <td style="border-right: 1px solid #000000; font-weight: bold; text-align: right;">{{ number_format($item->jumlah_1, 0, ',', '.') }}</td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS 13: Jarak Kosong --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="9" style="border-left: 1px solid #000000; border-right: 1px solid #000000;">&nbsp;</td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- ========================================================== --}}
+        {{-- BAGIAN 3: POTONGAN --}}
+        {{-- ========================================================== --}}
+
+        {{-- BARIS 14: Header Potongan --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="9" style="border-left: 1px solid #000000; border-right: 1px solid #000000; font-weight: bold; text-decoration: underline;">POTONGAN</td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS 15: Absensi Hari --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="4" style="border-left: 1px solid #000000;">ABSENSI / HARI</td>
+                <td colspan="2" style="border: 1px solid #000000; text-align: center;">{{ $item->absen_hari ?? 0 }}</td>
+                <td style="border: 1px solid #000000; text-align: right;">180.000</td>
+                <td style="text-align: center;">=</td>
+                <td style="border-right: 1px solid #000000; text-align: right;">{{ number_format($item->potongan_hari ?? 0, 0, ',', '.') }}</td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS 16: Absensi Jam --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="4" style="border-left: 1px solid #000000;">ABSENSI / JAM</td>
+                <td colspan="2" style="border: 1px solid #000000; text-align: center;">{{ $item->absen_jam ?? 0 }}</td>
+                <td style="border: 1px solid #000000; text-align: right;">26.012</td>
+                <td style="text-align: center;">=</td>
+                <td style="border-right: 1px solid #000000; text-align: right;">{{ number_format($item->potongan_jam ?? 0, 0, ',', '.') }}</td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS 17: BPJS TK --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="7" style="border-left: 1px solid #000000;">BPJS KETENAGAKERJAAN</td>
+                <td style="text-align: center;">=</td>
+                <td style="border-right: 1px solid #000000; text-align: right;">({{ number_format($item->bpjs_tk ?? 0, 0, ',', '.') }})</td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS 18: BPJS Kes --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="7" style="border-left: 1px solid #000000;">BPJS KESEHATAN</td>
+                <td style="text-align: center;">=</td>
+                <td style="border-right: 1px solid #000000; text-align: right;">({{ number_format($item->bpjs_kes ?? 0, 0, ',', '.') }})</td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS 19: Klaim --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="7" style="border-left: 1px solid #000000;">BIAYA KLAIM</td>
+                <td style="text-align: center;">=</td>
+                <td style="border-right: 1px solid #000000; text-align: right;">-</td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS 20: Lain-lain --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="7" style="border-left: 1px solid #000000;">LAIN - LAIN</td>
+                <td style="text-align: center;">=</td>
+                <td style="border-right: 1px solid #000000; text-align: right;">(0)</td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS 21: Spacer Garis Ganda --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="8" style="border-left: 1px solid #000000;"></td>
+                <td style="border-right: 1px solid #000000; border-bottom: 3px double #000000;"></td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS 22: Jumlah 2 --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="7" style="border-left: 1px solid #000000; font-weight: bold; font-style: italic; text-align: center;">JUMLAH 2</td>
+                <td></td>
+                <td style="border-right: 1px solid #000000; font-weight: bold; text-align: right;">({{ number_format($item->jumlah_2 ?? 0, 0, ',', '.') }})</td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- ========================================================== --}}
+        {{-- BAGIAN 4: FOOTER --}}
+        {{-- ========================================================== --}}
+
+        {{-- BARIS 23: Take Home Pay --}}
+        <tr>
+            @foreach($group as $item)
+                <td colspan="8" style="border: 1px solid #000000; font-weight: bold; font-size: 12pt;">TAKE HOME PAY</td>
+                <td style="border: 1px solid #000000; font-weight: bold; font-size: 12pt; text-align: right;">
+                    {{ number_format($item->take_home_pay, 0, ',', '.') }}
+                </td>
+                <td></td>
+            @endforeach
+        </tr>
+
+        {{-- BARIS SPASI VERTIKAL ANTAR GRUP --}}
+        <tr><td colspan="30" style="height: 30px;"></td></tr>
+
+    @endforeach
+</table>
