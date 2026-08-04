100b778200206c53b6bca9e1cc82502230e4b275
Sansan <sani.aliem@gmail.com>
2026-08-04 13:27:40 +0700

    Update PasswordResetLinkController.php

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -10,7 +10,7 @@ class PasswordResetLinkController extends Controller
 {
     public function create()
     {
-        return view('auth.forgot-password');
+        return view('Auth.forgot-password');
     }
 
     public function store(Request $request)
