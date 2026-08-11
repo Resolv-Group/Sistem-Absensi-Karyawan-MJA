d519916bc1ccec11a00e82dc3c7a0ee5e32244d9
NigelKus <143382300+NigelKus@users.noreply.github.com>
2026-08-11 13:19:18 +0700

    a

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            buildDirectory: 'dist',
+            buildDirectory: 'public/build',
         }),
         tailwindcss(),
     ],
